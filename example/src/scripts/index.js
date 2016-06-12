8f555ad9c2e72126c2ea56a72d4128bf8b4890f3
Patrick Grimard <pgrimard@gmail.com>
2016-06-12 17:32:51 -0400

    Added example.

---
+++ example/src/scripts/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import TimePicker from 'react-time-picker';
+import 'styles/main.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <TimePicker name="myTimePicker"/>
+      </div>
+    );
+  }
+}
+
+render(<App/>, document.getElementById("app"));
---
+++ example/webpack.config.babel.js
@@ -0,0 +1,42 @@
+import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import LiveReloadPlugin from 'webpack-livereload-plugin';
+
+export default {
+  entry: {
+    main: './src/scripts/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.css'],
+    alias: {
+      'styles': __dirname + '/src/styles',
+      'components': __dirname + '/src/scripts'
+    }
+  },
+  module: {
+    loaders: [
+      {test: /\.scss$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')},
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css'),
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: 'src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+      minify: {
+        collapseWhitespace: true
+      }
+    }),
+    new LiveReloadPlugin()
+  ]
+}
