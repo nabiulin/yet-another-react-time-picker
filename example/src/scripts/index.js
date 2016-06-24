48441d7bfa2e9fcb9b48764d1383fe52c54f88c5
Patrick Grimard <pgrimard@gmail.com>
2016-06-24 11:54:19 -0400

    Updated example.

--- example/src/scripts/index.js
+++ example/src/scripts/index.js
@@ -4,10 +4,27 @@ import TimePicker from 'yet-another-react-time-picker';
 import 'styles/main.scss';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    };
+  }
+
+  handleTimeChange = (h, m, s) => {
+    this.setState({hours: h, minutes: m, seconds: s});
+  };
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
   render() {
     return (
       <div>
-        <TimePicker name="myTimePicker"/>
+        <TimePicker name="myTimePicker" onChange={this.handleTimeChange}/>
       </div>
     );
   }
