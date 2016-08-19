f08f70d934cd0cd95ada2b5abd255fa04ed99499
Patrick Grimard <pgrimard@gmail.com>
2016-08-19 14:18:14 -0400

    Added support for maxHour, maxMinute and maxSecond.
    Updated version to 2.2.0

--- example/src/scripts/index.js
+++ example/src/scripts/index.js
@@ -10,7 +10,10 @@ class App extends React.Component {
     this.state = {
       hours: 0,
       minutes: 0,
-      seconds: 0
+      seconds: 0,
+      anotherHours: 0,
+      anotherMinutes: 0,
+      anotherSeconds: 0
     };
   }
 
@@ -18,15 +21,32 @@ class App extends React.Component {
     this.setState({hours: h, minutes: m, seconds: s});
   };
 
+  handleAnotherTimeChange = (h, m, s) => {
+    this.setState({anotherHours: h, anotherMinutes: m, anotherSeconds: s});
+  };
+
   render() {
     return (
       <div>
+        <h2>Basic Usage</h2>
         <TimePicker
           hour={this.state.hours}
           minute={this.state.minutes}
           second={this.state.seconds}
           name="myTimePicker"
           onChange={this.handleTimeChange}
+        />
+        <h2>Max Time</h2>
+        <p>The max time on this TimePicker is 22:18:25.</p>
+        <TimePicker
+          hour={this.state.anotherHours}
+          maxHour={22}
+          minute={this.state.anotherMinutes}
+          maxMinute={18}
+          second={this.state.anotherSeconds}
+          maxSecond={25}
+          name="anotherTimePicker"
+          onChange={this.handleAnotherTimeChange}
         />
       </div>
     );
